4a61b4231de5fd9d88aebc05fdc80f65f197413e
734maboy <supamem74@gmail.com>
2019-11-13 19:54:42 +0300

    project init

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,32 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <router-link class="white--text" to="/settings">
+        <v-btn text> Настройки </v-btn>
+      </router-link>
+      <router-link class="white--text" to="/about">
+        <v-btn text> О нас </v-btn>
+      </router-link>
+      <router-link class="white--text" to="/load">
+        <v-btn text> Загрузить изменения </v-btn>
+      </router-link>
+    </v-app-bar>
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    //
+  })
 }
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+</script>
---
+++ src/views/Settings.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-layout>
+    <h1>This is an Settings page</h1>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
